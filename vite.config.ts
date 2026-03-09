06271225a72551d4641957def737e98062d25b18
rodrigosimoes97 <165928813+rodrigosimoes97@users.noreply.github.com>
2026-03-09 13:37:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Ajuste este valor para o nome do seu repositório no GitHub Pages.
-// Ex.: https://usuario.github.io/church-pad-player/ => base: '/church-pad-player/'
 const repoName = 'pad';
 
 export default defineConfig({
